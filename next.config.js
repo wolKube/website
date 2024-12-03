725a54a2bff449ffccd6977f837e27b4f1f7615e
Siamak Haschemi <siamak@i19s.com>
2024-12-03 19:27:10 +0100

    no image optimization

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   output: 'export',
   images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
